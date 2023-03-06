5a991e85308097c2770bc21b4d2985df9d874632
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-03-06 04:06:33 -0800

    Activities added

---
+++ app/src/main/java/com/example/finalprojecttbusche/HowToPlay.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojecttbusche
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HowToPlay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_to_play)
+    }
+}
