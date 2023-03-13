4fb62fc2faebf3c2c32bd406408cbfd2aaef9eca
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2023-03-12 20:23:09 -0700

    Main to Login and reverse set up

--- app/src/main/java/com/example/finalprojecttbusche/LoginScreen.kt
+++ app/src/main/java/com/example/finalprojecttbusche/LoginScreen.kt
@@ -1,11 +1,21 @@
 package com.example.finalprojecttbusche
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        // Button
+        // Navigate to Login Screen: activity_login_screen.xml & StudentInfo.kt
+        findViewById<ImageButton>(R.id.loginToMainBtn).setOnClickListener {
+            val submitOnlineIntent = Intent(this, MainActivity::class.java)
+            startActivity(submitOnlineIntent)
+        }
     }
 }
--- app/src/main/java/com/example/finalprojecttbusche/MainActivity.kt
+++ app/src/main/java/com/example/finalprojecttbusche/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.finalprojecttbusche
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Button
+        // Navigate to Login Screen: activity_login_screen.xml & StudentInfo.kt
+        findViewById<Button>(R.id.mainToLoginBtn).setOnClickListener {
+            val submitOnlineIntent = Intent(this, LoginScreen::class.java)
+            startActivity(submitOnlineIntent)
+        }
     }
 }
